6554421496ce674ac24b94ff3a949a73a0b52aac
Salsabaga <dbaldeonabril@gmail.com>
2021-03-20 23:41:15 +0000

    moved files to appropiate directories, added post api command but needs work

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
+app.use(express.static("static"))
+app.use(express.json())
+
 
 app.get("/", (req, res) => {
-	res.send("Hello World");
+	res.sendFile(__dirname + "/index.html");
+
 });
 
-app.post("/", (req, res) => {});
+app.post("/", (req, res) => {
+	const timerSaved = req.body;
+	console.log(timerSaved);
+	res.json({
+		status: "Success",
+		times: timerSaved,
+	})
+});
 
 app.listen(3000, () => {
 	console.log("App is listening");
--- static/app.js
+++ static/app.js
@@ -1,3 +1,10 @@
+//
+
+
+const savedTimes = [];
+const timer = document.querySelector("#timer-zone span");
+const timerInfo = timer.innerHTML;
+const saveBtn = document.querySelector("button#test");
 let timeBegan = null,
 	timeStopped = null,
 	stoppedDuration = 0,
@@ -5,17 +12,16 @@ let timeBegan = null,
 
 // You should keep track of the starting time then subtract that time from the current time using a Date:
 // The reason you were seeing the milliseconds "lagging" before was that setInterval is notorious for not firing exactly when you specify.
-
 // Update: You could keep track of how long the timer has "paused" between resets. Updated my answer to accommodate this.
 
-function clockTimer() {
+async function clockTimer() {
 	let currentTime = new Date(),
 		timeElapsed = new Date(currentTime - timeBegan - stoppedDuration),
-		min = timeElapsed.getUTCMinutes(),
-		sec = timeElapsed.getUTCSeconds(),
-		milisecs = timeElapsed.getUTCMilliseconds();
+		min = await timeElapsed.getUTCMinutes(),
+		sec = await timeElapsed.getUTCSeconds(),
+		milisecs = await timeElapsed.getUTCMilliseconds();
 
-	document.getElementById("timer-zone").innerHTML =
+	document.querySelector("#timer-zone span").innerHTML =
 		(min > 9 ? min : " 0" + min) +
 		" : " +
 		(sec > 9 ? sec : " 0" + sec) +
@@ -50,14 +56,32 @@ document.getElementById("reset").addEventListener("click", () => {
 	timeBegan = null;
 	timeStopped = null;
 	stoppedDuration = 0;
-	document.getElementById("timer-zone").innerHTML = "Lets Start Again";
+	document.querySelector("#timer-zone span").innerHTML = "Lets Start Again";
 });
 
 document.getElementById("save").addEventListener("click", () => {
 	let node = document.createElement("LI"); // Create a <li> node
-	let p = document.getElementById("timer-zone").value;
+	let p = document.querySelector("#timer-zone span").innerHTML;
 	let textnode = document.createTextNode(p); // Create a text node
 	node.appendChild(textnode);
 	document.getElementById("saved-times").appendChild(node);
+	savedTimes.push(document.querySelector("#timer-zone span").innerHTML);
+	console.log(document.querySelector("#timer-zone span").innerHTML);
 });
-//
+const options = {
+	method: "POST",
+	headers: { "Content-Type": "application/json" },
+	body: JSON.stringify({
+		time: timerInfo,
+	}),
+};
+
+const timerAPI = async () => {
+	const res = await fetch("/", options);
+	const data = await res.json();
+	console.log(data);
+};
+
+saveBtn.addEventListener("click", () => {
+	timerAPI();
+});
